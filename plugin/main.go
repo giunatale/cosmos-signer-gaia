eaea893b70e0f3b76ad0cf7b72ca69774903b8a7
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2024-04-25 22:55:29 +0200

    plugin conversion

---
+++ plugin/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+var Cosmos_bank_v1beta1_RegisterLegacyAminoCodec = banktypes.RegisterLegacyAminoCodec
+var Cosmos_bank_v1beta1_RegisterInterfaces = banktypes.RegisterInterfaces
+
+func main() {}
